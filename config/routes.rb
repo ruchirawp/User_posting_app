4d0eb60ff98728df8e717758ebe6c7727aa6810f
Ruchira <ruchirawperera@gmail.com>
2020-05-27 03:07:23 +0000

    Minipost App

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :microposts
+  root 'microposts#index'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#testing2'
+  #root 'users#index'
 end
